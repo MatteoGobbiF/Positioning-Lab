b851f453f9abb85a7c318863c893061e663d280d
Matteo Gobbi Frattini <matteo.gf@live.it>
2023-11-01 15:44:01 +0100

    First commit ex 4

---
+++ 4/Ex4.m
@@ -0,0 +1,25 @@
+run('Ex04_variables.m');
+
+x0 = [0,0,0];
+clockOffRe = 0;
+c = s_light;
+maxIterations = 10;
+aed_sat = zeros(length(xyz_sat),1);
+
+xyz_rec=x0;
+for i = 1:1
+    
+    [azimuth, elevation, rho] = topocent(xyz_rec, xyz_sat);    
+    
+    xGeo = gc2gg(xyz_rec);
+    xGeo(3) = NaN;
+
+    tropoC = tropo_correction (xGeo(3), elevation);
+    ionoC = iono_correction(xGeo(1), xGeo(2), azimuth, elevation, time_rx, ionoparams);
+    
+    deltaP = pr_C1 - rho - tropoC - ionoC + c*dtS;
+    
+
+
+end
+
